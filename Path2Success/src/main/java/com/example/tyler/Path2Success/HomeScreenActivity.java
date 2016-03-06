ab76d0b0bbb0275f5d96b56f546de7eb438746e6
pbertel <pbertel@macalester.edu>
2016-03-05 19:04:43 -0600

    Merged in changes from local-storage and refactoring branches. To be later merged with master, although small bugs are present (layout styling is odd)

--- Path2Success/src/main/java/com/example/tyler/Path2Success/HomeScreenActivity.java
+++ Path2Success/src/main/java/com/example/tyler/Path2Success/HomeScreenActivity.java
@@ -20,20 +20,37 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Date;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.tyler.myfirstapp.MESSAGE";
     public final static String EXTRA_MESSAGE2 = "com.example.tyler.myfirstapp.MESSAGE2";
     private ListView mLayout;
+    public static final String FILENAME = "goal_file";
     private EditText mText1;
     private EditText mText2;
     private Button mButton;
     private LayoutTransition mTransition;
     private ArrayList<IndividualGoal> itemListHolder=new ArrayList<>();
     //private ArrayList<String> itemListHolder =new ArrayList<>();
-   private GoalDataAdapter adapter;
+    private GoalDataAdapter adapter;
     //private ArrayAdapter<String> adapter;
+    private JSONArray goalList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +70,62 @@ public class HomeScreenActivity extends AppCompatActivity {
         setTitle("Path 2 Success");
         adapter=new GoalDataAdapter(this,itemListHolder);
         mLayout.setAdapter(adapter);
+
+        //add prior goals stored locally
+        goalList = new JSONArray();
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+
+            //Code used from http://chrisrisner.com/31-Days-of-Android--Day-23-Writing-and-Reading-Files/
+        try {
+
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            StringBuffer b = new StringBuffer();
+            while (bis.available() != 0) {
+                char c = (char) bis.read();
+                b.append(c);
+            }
+            bis.close();
+            fis.close();
+
+//            JSONArray goals = new JSONArray(b.toString());
+            goalList = new JSONArray(b.toString());
+
+//            String text = goals.getJSONObject(0).getString("title");
+
+            Toast toast = Toast.makeText(context, String.valueOf(goalList.length()), duration);
+//            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            for (int i = 0; i < goalList.length(); i++) {
+                String title = goalList.getJSONObject(i).getString("title");
+                String date = goalList.getJSONObject(i).getString("date");
+
+                //here add each corresponding checkbox to the view
+
+//                CheckBox cBox = createNewCheckBox(title + " " + date);
+//                cBox.setOnClickListener(onClickBox(cBox));
+//                mLayout.addView(cBox);
+//                mTransition.addChild(mLayout,cBox);
+                IndividualGoal newGoal = new IndividualGoal(title, date);
+                itemListHolder.add(newGoal);
+                adapter.notifyDataSetChanged();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        //on load, we want to read all of the task objects into the view
+        //load them in the same way they are printed in the AddNewItem way
+        //maybe make a new method (that comes from this onCreate method) that adds
+        //    the previously stored goals to the view
     }
 
 //
@@ -71,12 +144,57 @@ public class HomeScreenActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                IndividualGoal newGoal = new IndividualGoal(mText1.getText().toString(),mText2.getText().toString());
-                itemListHolder.add(newGoal);
-                adapter.notifyDataSetChanged();
 //                CheckBox cBox = createNewCheckBox(mText1.getText().toString() + " " + mText2.getText().toString());
 //                cBox.setOnClickListener(onClickBox(cBox));
 //                mLayout.addFooterView(cBox);
+//                mTransition.addChild(mLayout,cBox);
+
+                //Making a test toast
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_LONG;
+
+                String title = mText1.getText().toString();
+                String date = mText2.getText().toString();
+
+                //add new goal to local storage
+//                String FILENAME = "goal_file";
+                try {
+                    //here is where you would make a javascript object and add it to local storage
+//                    JSONArray goalList = new JSONArray();
+                    JSONObject goal;
+
+                    goal = new JSONObject();
+                    goal.put("title", title);
+                    goal.put("date", date);
+                    goalList.put(goal);
+
+                    String goals = goalList.toString();
+
+                    FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+//                    String combinedString = title + " " + date;
+                    fos.write(goals.getBytes());
+                    fos.close();
+
+                    //Making a test toast
+                    String text = "Goal successfully written to device!\n" + goals;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                IndividualGoal newGoal = new IndividualGoal(title, date);
+                itemListHolder.add(newGoal);
+                adapter.notifyDataSetChanged();
+
+
+//                CheckBox cBox = createNewCheckBox(mText1.getText().toString() + " " + mText2.getText().toString());
+//                cBox.setOnClickListener(onClickBox(cBox));
+//                mLayout.addView(cBox);
 //                mTransition.addChild(mLayout,cBox);
 
                 //myLayout.addView
