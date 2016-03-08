d0041d485791b74baeea27fa9b7046abf89ccfd2
Alison Chi <achi@macalester.edu>
2016-03-07 19:50:10 -0600

    New class for new screen

--- Path2Success/src/main/java/com/example/tyler/Path2Success/InputNewGoal.java
+++ Path2Success/src/main/java/com/example/tyler/Path2Success/InputNewGoal.java
@@ -1,7 +1,13 @@
 package com.example.tyler.Path2Success;
 
-/**
- * Created by Alison on 3/7/2016.
- */
-public class InputNewGoal {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputNewGoal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_new_goal);
+    }
 }
