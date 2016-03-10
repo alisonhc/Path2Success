e8b4f5c736a878968b2a2abd42bc906a8e1061a3
angelicalin <angelicalin7@gmail.com>
2016-03-09 20:10:47 -0600

    Made datepicker a lot nicer

--- Path2Success/src/main/java/com/example/tyler/Path2Success/InputNewGoal.java
+++ Path2Success/src/main/java/com/example/tyler/Path2Success/InputNewGoal.java
@@ -1,6 +1,7 @@
 package com.example.tyler.Path2Success;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,33 +10,73 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class InputNewGoal extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.tyler.myfirstapp.MESSAGE";
     public final static String EXTRA_MESSAGE2 = "com.example.tyler.myfirstapp.MESSAGE2";
     private Button addButton;
-    private DatePicker dueDate;
+    //private DatePicker dueDate;
     private EditText taskContent;
+    private EditText dateInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_new_goal);
         addButton = (Button) findViewById(R.id.add_and_back);
-        dueDate = (DatePicker) findViewById(R.id.datePicker);
+        //dueDate = (DatePicker) findViewById(R.id.datePicker);
         taskContent = (EditText) findViewById(R.id.taskContent);
+        dateInput = (EditText) findViewById(R.id.datePicker);
+        dateInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                pickDate();
+            }
+        });
     }
 
     public void addNewItem(View view){
         Intent intent = new Intent();
         String task = taskContent.getText().toString();
-        String date = Integer.toString(dueDate.getMonth()+1) + "/" + Integer.toString(dueDate.getDayOfMonth());
+        //String date = Integer.toString(dueDate.getMonth()+1) + "/" + Integer.toString(dueDate.getDayOfMonth());
+        String date = dateInput.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, task);
         intent.putExtra(EXTRA_MESSAGE2, date);
         setResult(Activity.RESULT_OK, intent);
         finish();
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+
+    public void pickDate(){
+        new DatePickerDialog(InputNewGoal.this, date,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        dateInput.setText(sdf.format(myCalendar.getTime()));
     }
 }
